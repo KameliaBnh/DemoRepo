adda46c5959e09de1e0983063fbec4b4e25a949d
Kamelia Benhamna <kamelia.benhamna.592@cranfield.ac.uk>
2023-02-27 14:51:20 +0000

    Update of GUI_TKinter.py: better layout of the different pannels

--- Pyhton TKinter/GUI_TKinter.py
+++ Pyhton TKinter/GUI_TKinter.py
@@ -1,45 +1,64 @@
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
-import os
+
+
+# Define the function to open a file
+def open_file():
+    # Filter acceptable file types
+    file_types = [('Image Files', '*.jpg;*.jpeg;*.png;*.gif')]
+
+    # Open a dialog to select a file or folder
+    file_path = filedialog.askopenfilename(filetypes=file_types)
+
+    # Display the selected image
+    img = Image.open(file_path)
+    img.thumbnail((400, 600))
+    photo = ImageTk.PhotoImage(img)
+    image_label.configure(image=photo)
+    image_label.image = photo
+
+# Define the function to export the report
+def export_report():
+    print("Exporting report...")
+
+
 
 # Create a Tkinter window
 root = tk.Tk()
+root.title("Automated Pollinator Monitoring")
+root.geometry("800x600") # Set the size of the window
 
-# Define the maximum size of the image for a 13-inch screen
-max_width = 1366
-max_height = 768
+# Create a menu bar
+menu_bar = tk.Menu(root)
+menu_bar.add_command(label="Open", command=open_file)
+menu_bar.add_command(label="Export report", command=export_report)
+root.config(menu=menu_bar)
 
-# Create a function to open a file or a folder
-def open_file():
-    # Filter acceptable file types
-    file_types = [('Image files', '*.jpg;*.jpeg;*.png;*.gif'), ('Folders', '*')]
-    
-    # Open a dialog box to select a file or folder
-    file_path = filedialog.askopenfilename(filetypes=file_types)
+# Create the frames
+image_frame = tk.Frame(root, width=400, height=580, bd=1, relief="sunken")
+options_frame = tk.Frame(root, width=380, height=280, bd= 1, relief="sunken")
+statistics_frame = tk.Frame(root, width=380, height=300, bd= 1, relief="sunken")
 
-    # Display the selected file
-    if os.path.isdir(file_path):
-        print('You selected a folder:', file_path)
-    else:
-        print('You selected a file:', file_path)
-        img = Image.open(file_path)
-        
-        # Resize the image if it exceeds the maximum size
-        if img.width > max_width or img.height > max_height:
-            ratio = min(max_width/img.width, max_height/img.height)
-            img = img.resize((int(img.width*ratio), int(img.height*ratio)))
-        
-        photo = ImageTk.PhotoImage(img)
-        label = tk.Label(root, image=photo)
-        label.image = photo
-        label.pack()
+# Create labels for each frame
+image_label = tk.Label(image_frame)
+options_label = tk.Label(options_frame, text="Options: ", padx=5, pady=5)
+statistics_label = tk.Label(statistics_frame, text="Statistics: ", padx=5, pady=5)
 
-# Create an "Open" button
-open_button = tk.Button(root, text="Open", command=open_file)
-open_button.pack()
+# Place the labels at the top left corner of each frame
+image_label.pack(side="top", anchor="nw")
+options_label.pack(side="top", anchor="nw")
+statistics_label.pack(side="top", anchor="nw")
+
+# Place the frames on the window
+image_frame.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+options_frame.grid(row=0, column=1, padx=5, pady=5, sticky="n")
+statistics_frame.grid(row=0, column=1, padx=5, pady=5, sticky="s")
+
+# Make frames fixed in size
+image_frame.pack_propagate(0)
+options_frame.pack_propagate(0)
+statistics_frame.pack_propagate(0)
 
 # Display the Tkinter window
 root.mainloop()
-
-# Now you have your graphical user interface!
